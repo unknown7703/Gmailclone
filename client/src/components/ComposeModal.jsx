f52f77df61f3e2d468dc064b391da9c74ae5c7c1
Aatreya <padmajaatreya02@gmail.com>
2023-08-21 04:14:21 +0530

    write mail

--- client/src/components/ComposeModal.jsx
+++ client/src/components/ComposeModal.jsx
@@ -1,5 +1,22 @@
-import { Dialog, Box ,styled,InputBase,Button,Typography} from "@mui/material";
-
+import {
+  Dialog,
+  Box,
+  styled,
+  InputBase,
+  Button,
+  Typography,
+} from "@mui/material";
+import { Close } from "@mui/icons-material";
+const SubjectWrapper =styled(Box)(
+    {
+        display:'flex',
+        flexDirection:'column',
+        padding:"0px 10px",
+        '&>div':{
+            borderBottom:'1px solid #F5F5F5',
+        }
+    }
+)
 const ComposeModalStyle = {
   height: "90%",
   width: "80%",
@@ -7,24 +24,29 @@ const ComposeModalStyle = {
   maxHeight: "100%",
   borderRadius: "10px 10px 0px 0px",
 };
-const Header =styled(Box)
-(
-    {
-        background:"#f2f6fc",
-        height:'3%',
-        padding:'10px 15px',
-        '&>p':{
-            fontSize:'small',
-            fontWeight:'500',
-        }
-    }
-)
+const Header = styled(Box)({
+  display: "flex",
+  flexDirection: "row",
+  background: "#f2f6fc",
+  height: "3%",
+  padding: "10px 15px",
+  "&>p": {
+    fontSize: "small",
+    fontWeight: "500",
+  },
+});
 const ComposeModal = ({ openCompose }) => {
   return (
     <Dialog open={openCompose} PaperProps={{ sx: ComposeModalStyle }}>
       <Header>
-        <Typography>New message</Typography>
+        <Typography width="100%">New message</Typography>
+        <Close fontSize="small" />
       </Header>
+      <SubjectWrapper>
+        <InputBase placeholder="Recipients" sx={{}}/>
+        <InputBase placeholder="Subject" />
+      </SubjectWrapper>
+      <Button/>
     </Dialog>
   );
 };

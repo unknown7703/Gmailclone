4be3453e577179bb2ba7ed2a702bff231cc93fe7
Aatreya <padmajaatreya02@gmail.com>
2023-09-06 04:06:10 +0530

    server made

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'; 
 const Connection =()=>
 {
-    const DB_URI=`mongodb://unknownred:39vpTaQmLEuK4R07@ac-1v9xcdl-shard-00-00.mk3yv1b.mongodb.net:27017,ac-1v9xcdl-shard-00-01.mk3yv1b.mongodb.net:27017,ac-1v9xcdl-shard-00-02.mk3yv1b.mongodb.net:27017/?ssl=true&replicaSet=atlas-57spmh-shard-0&authSource=admin&retryWrites=true&w=majority`
+   
     try {
         mongoose.connect(DB_URI,{useNewUrlParser: true});
         console.log('connected to DB')
